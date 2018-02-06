7fbf0545909c558669b86ccdeb29220ce1117644
Brian J. Cardiff <bcardiff@gmail.com>
2018-02-06 13:55:33 -0300

    Update shards repo url

--- omnibus/config/software/shards.rb
+++ omnibus/config/software/shards.rb
@@ -6,7 +6,7 @@ default_version SHARDS_VERSION
 dependency "crystal"
 dependency "libyaml"
 
-source url: "https://github.com/ysbaddaden/shards/archive/v#{version}.tar.gz",
+source url: "https://github.com/crystal-lang/shards/archive/v#{version}.tar.gz",
        md5: "4f1f1e860ed1846fce01581ce9e6e7ad"
 
 relative_path "shards-#{version}"
