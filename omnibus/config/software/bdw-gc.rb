c1823a8ec8fe3afba1ecc6fdfbe30019e9a6e570
Brian J. Cardiff <bcardiff@gmail.com>
2018-09-04 14:01:37 -0300

    Update to bdw-gc v7.6.8 and libatomic_ops v7.6.6

--- omnibus/config/software/bdw-gc.rb
+++ omnibus/config/software/bdw-gc.rb
@@ -1,8 +1,15 @@
 name "bdw-gc"
-default_version "7.4.10"
+default_version "7.6.8"
 
-source :url => "https://github.com/ivmai/bdwgc/releases/download/v#{version}/gc-#{version}.tar.gz",
-       :md5 => "6d894c05c218aa380cd13f54f9c715e9"
+source :url => "https://github.com/ivmai/bdwgc/releases/download/v#{version}/gc-#{version}.tar.gz"
+
+version "7.4.10" do
+  source md5: "6d894c05c218aa380cd13f54f9c715e9"
+end
+
+version "7.6.8" do
+  source md5: "9ae6251493ead5d0d13b044954cec7d7"
+end
 
 dependency "libatomic_ops"
 
--- omnibus/config/software/libatomic_ops.rb
+++ omnibus/config/software/libatomic_ops.rb
@@ -1,8 +1,15 @@
 name "libatomic_ops"
-default_version "7.4.10"
+default_version "7.6.6"
 
-source :url => "https://github.com/ivmai/libatomic_ops/releases/download/v#{version}/libatomic_ops-#{version}.tar.gz",
-       :md5 => "47dcc36e1f77ab0efdee5d99888b3ffe"
+source :url => "https://github.com/ivmai/libatomic_ops/releases/download/v#{version}/libatomic_ops-#{version}.tar.gz"
+
+version "7.4.10" do
+  source md5: "47dcc36e1f77ab0efdee5d99888b3ffe"
+end
+
+version "7.6.6" do
+  source md5: "4514d85d14e21af05d59877c9ded5abc"
+end
 
 relative_path "libatomic_ops-#{version}"
 
