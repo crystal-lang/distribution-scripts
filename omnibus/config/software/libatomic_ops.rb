d0be4da404616dde24ec9057210d1a62ae77351e
Brian J. Cardiff <bcardiff@gmail.com>
2019-03-25 15:18:20 -0300

    GC MT Support. Upgrade bdw-gc v7.6.12 and libatomic_ops v7.6.10
    * remove fix-gcf-memory-allocation.patch already included in v.7.6.12
    * include thread-statckbottom.patch for GC MT

--- omnibus/config/software/libatomic_ops.rb
+++ omnibus/config/software/libatomic_ops.rb
@@ -1,5 +1,5 @@
 name "libatomic_ops"
-default_version "7.6.6"
+default_version "7.6.10"
 
 source :url => "https://github.com/ivmai/libatomic_ops/releases/download/v#{version}/libatomic_ops-#{version}.tar.gz"
 
@@ -9,6 +9,10 @@ end
 
 version "7.6.6" do
   source md5: "4514d85d14e21af05d59877c9ded5abc"
+end
+
+version "7.6.10" do
+  source md5: "90a78a84d9c28ce11f331c25289bfbd0"
 end
 
 relative_path "libatomic_ops-#{version}"
