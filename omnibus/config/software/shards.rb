20f8e5e5eecb1aa2b6599e85f4efdb06b4e923ec
Brian J. Cardiff <bcardiff@gmail.com>
2020-06-05 14:44:45 -0300

    Update shards v0.11.0

--- omnibus/config/software/shards.rb
+++ omnibus/config/software/shards.rb
@@ -1,4 +1,4 @@
-SHARDS_VERSION = "0.10.0"
+SHARDS_VERSION = "0.11.0"
 
 name "shards"
 default_version SHARDS_VERSION
@@ -20,6 +20,10 @@ end
 
 version "0.10.0" do
   source md5: "f982f2dc0c796763205bd0de68e9f87e"
+end
+
+version "0.11.0" do
+  source md5: "a16d6580411152956363a766e1517c9d"
 end
 
 source url: "https://github.com/crystal-lang/shards/archive/v#{version}.tar.gz"
