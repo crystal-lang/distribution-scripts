20daeb8681c29c1fac0260927545d0155b02f759
Brian J. Cardiff <bcardiff@gmail.com>
2020-06-26 15:17:18 -0300

    Add repo creation targets

--- bintray/rebuild.sh
+++ bintray/rebuild.sh
@@ -6,14 +6,6 @@ function rebuild () {
   crystal_version=$1
   crystal_version_date=$2
 
-  # make all \
-  #       CRYSTAL_VERSION=$crystal_version \
-  #       CRYSTAL_VERSION_DATE=$crystal_version_date \
-  #       CRYSTAL_LINUX64_TARGZ=https://github.com/crystal-lang/crystal/releases/download/$crystal_version/crystal-$crystal_version-1-linux-x86_64.tar.gz \
-  #       CRYSTAL_LINUX32_TARGZ=https://github.com/crystal-lang/crystal/releases/download/$crystal_version/crystal-$crystal_version-1-linux-i686.tar.gz
-
-  # make rpm_publish force=1 \
-  # make publish \
   make publish set_version_date force=1 \
         CRYSTAL_VERSION=$crystal_version \
         CRYSTAL_VERSION_DATE=$crystal_version_date
@@ -29,3 +21,5 @@ rebuild 0.32.0 2019-12-11
 rebuild 0.32.1 2019-12-18
 rebuild 0.33.0 2020-02-14
 rebuild 0.34.0 2020-04-06
+rebuild 0.35.0 2020-06-09
+rebuild 0.35.1 2020-06-19
