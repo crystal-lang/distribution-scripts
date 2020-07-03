985348f0ea121e96f0aef0eab4a3827faebe1a54
Brian J. Cardiff <bcardiff@gmail.com>
2020-07-03 09:56:25 -0300

    WIP rpm setup script with version lookup

--- bintray/scripts/rpm/setup.sh
+++ bintray/scripts/rpm/setup.sh
@@ -12,13 +12,55 @@
 # * Run as root
 #
 
-DISTRO:=el6
-DISTRO=${2:-el6}
+set -eu
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root!"
+  exit 1
+fi
+
+CRYSTAL_VERSION="latest"
+CHANNEL="stable"
+
+for i in "$@"
+do
+case $i in
+    --crystal=*)
+    CRYSTAL_VERSION="${i#*=}"
+    shift
+    ;;
+    --channel=*)
+    CHANNEL="${i#*=}"
+    shift
+    ;;
+    *)
+    echo "Invalid option $i"
+    ;;
+esac
+done
 
 cat > /etc/yum.repos.d/crystal.repo <<END
 [crystal]
-name = Crystal
-baseurl = https://dl.bintray.com/crystal/rpm/el6.centos
+name=Crystal
+baseurl=https://dl.bintray.com/crystal/rpm/el6
+gpgcheck=0
+repo_gpgcheck=1
+gpgkey=http://bintray.com/user/downloadSubjectPublicKey?username=bintray
 END
 
-rpm --import http://dist.crystal-lang.org/rpm/RPM-GPG-KEY
+# Install Crystal
+case "$CRYSTAL_VERSION" in
+  latest)
+    yum install -y crystal
+    ;;
+  *)
+    command -v repoquery >/dev/null || yum install -y yum-utils
+    CRYSTAL_PACKAGE=$(repoquery crystal-$CRYSTAL_VERSION* | tail -n1)
+    if [ -z "$CRYSTAL_PACKAGE" ]
+    then
+      echo "ERROR: Unable to find a package for crystal $CRYSTAL_VERSION"
+    else
+      yum install -y $CRYSTAL_PACKAGE
+    fi
+    ;;
+esac
