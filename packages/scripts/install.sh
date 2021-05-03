5c01e26885f68e7d23b358fb87ef13c53655c236
Johannes Müller <straightshoota@gmail.com>
2021-05-03 15:20:56 +0200

    Add support for generic deb/rpm distros

--- packages/scripts/install.sh
+++ packages/scripts/install.sh
@@ -106,12 +106,36 @@ _discover_distro_repo() {
       _check_version_id
       DISTRO_REPO="openSUSE_Leap_${VERSION_ID}"
       ;;
-    *)
+    "")
       _error "Unable to identify distribution. You may specify one with environment variable DISTRO_REPO"
       _error "Please, report to https://forum.crystal-lang.org/c/help-support/11"
       exit 1
       ;;
+    *)
+      # If there's no dedicated repository for the distro, try to figure out
+      # if the distro is apt or rpm based and use a default repository.
+      _discover_distro_type
+
+      case "$DISTRO_TYPE" in
+      deb)
+        DISTRO_REPO="Debian_Unstable"
+        ;;
+      rpm)
+        DISTRO_REPO="RHEL_7"
+        ;;
+      *)
+        _error "Unable to identify distribution type ($ID). You may specify a repository with the environment variable DISTRO_REPO"
+        _error "Please, report to https://forum.crystal-lang.org/c/help-support/11"
+        exit 1
+        ;;
+      esac
   esac
+}
+
+_discover_distro_type() {
+  DISTRO_TYPE=""
+  [[ $(command -v apt-get) ]] && DISTRO_TYPE="deb" && return
+  [[ $(command -v yum) ]]     && DISTRO_TYPE="rpm" && return
 }
 
 if [[ $EUID -ne 0 ]]; then
