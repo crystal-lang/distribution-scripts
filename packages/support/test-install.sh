35cc50225b71e1b2d498fcc48c8a87192401478e
Johannes Müller <straightshoota@gmail.com>
2021-05-03 20:24:51 +0200

    Fix packages test script to use deb noninteractive

--- packages/support/test-install.sh
+++ packages/support/test-install.sh
@@ -6,6 +6,9 @@ DISTRO_TYPE=""
 [[ -x "/usr/bin/apt-get" ]] && DISTRO_TYPE="deb"
 [[ -x "/usr/bin/yum" ]]     && DISTRO_TYPE="rpm"
 
+# Required for Ubuntu <= 20.04 to not ask for input during package installation
+export DEBIAN_FRONTEND=noninteractive
+
 ../scripts/install.sh
 crystal --version
 shards --version
