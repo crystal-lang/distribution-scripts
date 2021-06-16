ca523ba9d382b16fb9b1a03ca843a70bd0161f48
Brian J. Cardiff <bcardiff@gmail.com>
2021-06-16 20:50:55 -0300

    Update llvm_bin to use 10.0.0-2 (LLVM_TARGETS_TO_BUILD="X86;AArch64")

--- omnibus/config/software/llvm_bin.rb
+++ omnibus/config/software/llvm_bin.rb
@@ -1,6 +1,6 @@
 name "llvm_bin"
 LLVM_VERSION = (ENV['LLVM_VERSION'] || "10.0.0").strip
-default_version "#{LLVM_VERSION}-1"
+default_version "#{LLVM_VERSION}-2"
 skip_transitive_dependency_licensing true
 
 if linux?
@@ -21,7 +21,8 @@ elsif (macos? || mac_os_x?) && _64_bit?
   when "6.0.1"
     source_md5 = "435beaff5e309921f4d87c275cad4e03"
   when "10.0.0"
-    source_md5 = "edccfa777cba6e160b19bd5b57b12c8f"
+    # source_md5 = "edccfa777cba6e160b19bd5b57b12c8f" # 10.0.0-1
+    source_md5 = "dc44dbc947b67c76e44df1c9e38df901" # 10.0.0-2
   else
     raise "llvm_bin #{LLVM_VERSION} not supported on osx"
   end
