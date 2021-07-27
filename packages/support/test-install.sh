a8dbf4d30e57310349b2f7bbf16adef6be327b56
Johannes Müller <straightshoota@gmail.com>
2021-07-27 14:44:12 +0200

    Refactor test-install to only check for single repository

--- packages/support/test-install.sh
+++ packages/support/test-install.sh
@@ -9,42 +9,7 @@ DISTRO_TYPE=""
 # Required for Ubuntu <= 20.04 to not ask for input during package installation
 export DEBIAN_FRONTEND=noninteractive
 
-../scripts/install.sh
-crystal --version
-shards --version
-crystal eval 'puts "Hello World!"'
-
-../scripts/install.sh --channel=unstable
-crystal --version
-shards --version
-crystal eval 'puts "Hello World!"'
-
-../scripts/install.sh --channel=nightly
-crystal --version
-shards --version
-crystal eval 'puts "Hello World!"'
-
-# OBS doesn't have any fully valid older releases yet, so skipping the following
-# checks for now.
-exit 0
-
-# Uninstall explicitly for downgrade
-case $DISTRO_TYPE in
-  deb)
-    apt -y remove crystal
-    ;;
-  *)
-    rpm -e crystal
-    ;;
-esac
-
-../scripts/install.sh --crystal=0.34
-crystal --version
-shards --version
-# Crystal < 0.35 raises execvp (which "pkg-config"): No such file or directory: No such file or directory (Errno)
-[[ $DISTRO_TYPE == "deb" ]] && crystal eval 'puts "Hello World!"'
-
-../scripts/install.sh --crystal=0.35
+../scripts/install.sh ${@}
 crystal --version
 shards --version
 crystal eval 'puts "Hello World!"'
