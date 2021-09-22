3ab3d11f8cb9204d76cbdc366a7d5a2741be4e25
Johannes Müller <straightshoota@gmail.com>
2021-09-22 17:47:48 +0200

    Fix test script

--- linux/test.sh
+++ linux/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bats
 
 @test "bundled tarball" {
-  docker run -v $(pwd)/build:/build -w /build debian /bin/sh -e -c '
+  docker run -v $(pwd)/build:/build debian /bin/sh -e -c '
     apt update && apt install -y gcc
-    tar -xf crystal*-bundled.tar.gz
+    tar -xf /build/crystal*-bundled.tar.gz
     crystal-*/bin/crystal eval "puts \"Hello World\""'
 }
