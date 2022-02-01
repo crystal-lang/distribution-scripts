f0d8ee74366816611095d61299a89df04ed4ce3e
Johannes Müller <straightshoota@gmail.com>
2022-02-01 21:29:35 +0100

    Push major and minor tags to docker (#179)
    * Push major and minor tags to docker
    
    * Update docker/apply-latest-tags.sh
    
    Co-authored-by: Beta Ziliani <beta@manas.tech>

--- docker/apply-latest-tags.sh
+++ docker/apply-latest-tags.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # This helper script promotes the docker images for the specified version
-# to "latest".
+# to "latest" as well as the major and minor release tags.
 #
 # Usage:
 #
@@ -15,11 +15,23 @@ apply_tag() {
 
   echo "Publishing ${base_tag} as ${new_tag}"
 
-  docker pull "${base_tag}"
   docker tag "${base_tag}" "${new_tag}"
   docker push "${new_tag}"
 }
 
 version=$1
+
+docker pull "crystallang/crystal:${version}"
+docker pull "crystallang/crystal:${version}-alpine"
+
+# Tag latest
 apply_tag "crystallang/crystal:${version}"        "crystallang/crystal:latest"
 apply_tag "crystallang/crystal:${version}-alpine" "crystallang/crystal:latest-alpine"
+
+# Tag minor (x.y)
+apply_tag "crystallang/crystal:${version}"        "crystallang/crystal:${version%.*}"
+apply_tag "crystallang/crystal:${version}-alpine" "crystallang/crystal:${version%.*}-alpine"
+
+# Tag major (x)
+apply_tag "crystallang/crystal:${version}"        "crystallang/crystal:${version%%.*}"
+apply_tag "crystallang/crystal:${version}-alpine" "crystallang/crystal:${version%%.*}-alpine"
