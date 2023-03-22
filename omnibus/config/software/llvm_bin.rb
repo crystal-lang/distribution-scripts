dfa3fc201636ddb659eafc23c9761bc646cf613b
Johannes Müller <straightshoota@gmail.com>
2023-03-22 09:55:52 +0100

    Update llvm_bin to LLVM 15

--- omnibus/config/software/llvm_bin.rb
+++ omnibus/config/software/llvm_bin.rb
@@ -1,32 +1,21 @@
 name "llvm_bin"
-LLVM_VERSION = (ENV['LLVM_VERSION'] || "10.0.0").strip
+LLVM_VERSION = (ENV['LLVM_VERSION'] || "15.0.7").strip
 default_version "#{LLVM_VERSION}-3"
 skip_transitive_dependency_licensing true
 
-if linux?
+if (macos? || mac_os_x?) && _64_bit?
   case LLVM_VERSION
-  when "3.9.1"
-    if _64_bit?
-      source_md5 = "cedaa626e3959b5ab467467e6dfb91fe"
-    else
-      source_md5 = "8b847e903163054196d3854122363b8b"
-    end
-  else
-    raise "llvm_bin #{LLVM_VERSION} not supported on linux"
-  end
-elsif (macos? || mac_os_x?) && _64_bit?
-  case LLVM_VERSION
-  when "3.9.1"
-    source_md5 = "9fb52b6a648e700f431b459586eb5403"
-  when "6.0.1"
-    source_md5 = "435beaff5e309921f4d87c275cad4e03"
   when "10.0.0"
     # source_md5 = "edccfa777cba6e160b19bd5b57b12c8f" # 10.0.0-1
     # source_md5 = "dc44dbc947b67c76e44df1c9e38df901" # 10.0.0-2
     source_md5 = "d32c4d28b8fc50efda3f451e0d8265ea" # 10.0.0-3 (universal darwin)
+  when "15.0.7"
+    source_md5 = "0ab0ffe63a0e72346a979d7a0e964b94" # 15.0.7-3 (universal darwin)
   else
     raise "llvm_bin #{LLVM_VERSION} not supported on osx"
   end
+else
+  raise "llvm_bin not supported"
 end
 
 source url: "http://crystal-lang.s3.amazonaws.com/llvm/llvm-#{version}-#{ohai['os']}-#{ohai['kernel']['machine']}.tar.gz",
