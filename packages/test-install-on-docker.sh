212d0540b82b46e3d9b2e25c055e79822574bf82
Johannes Müller <straightshoota@gmail.com>
2023-11-13 15:17:54 +0100

    Make sure to pull current docker images before testing

--- packages/test-install-on-docker.sh
+++ packages/test-install-on-docker.sh
@@ -4,4 +4,5 @@ set -eu
 
 docker_image="${1}"
 
+docker pull "$docker_image"
 docker run -e OBS_PROJECT="${OBS_PROJECT:-}" -e CRYSTAL_VERSION="${CRYSTAL_VERSION:-}" --rm -it -v $(pwd)/scripts:/scripts -v $(pwd)/support:/support $docker_image /bin/sh -c "/support/test-install.sh ${@:2}"
