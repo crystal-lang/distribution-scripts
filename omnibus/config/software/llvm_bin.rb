8f2ab598cc68900041b2e590e0b7db7fe5ead755
Michael Nikitochkin <michael.nikitochkin@gmx.net>
2024-01-04 23:35:54 +0100

    llvm_bin uses same universal darwin archive for arm as for x86_64.
    Allow to override target architecture sufix in archive name in tgz_package.
    tgz_package uses build platform for naming archives.
    When build packages for multiple architectures like x86_64 and arm, then the archive name is not correct.
    Update the script of tgz_package to check for configuration option `ohai['target_arch']` as target arch name.
    
    It allows to specify architecture in project like:
    
    ```
    ohai['target_arch'] = 'universal'
    dependency 'tgz_package'
    ```
    
    Presenting GitHub Actions as an efficient solution for building LLVM from the ground up,
    seamlessly replicating the functionality of CircleCI.
    Unlike the extended duration of the CircleCI task, a new GitHub Actions workflow ensures a comprehensive build of LLVM,
    addressing time limitations and guaranteeing the successful completion of the package build process.

--- omnibus/config/software/llvm_bin.rb
+++ omnibus/config/software/llvm_bin.rb
@@ -18,7 +18,11 @@ else
   raise "llvm_bin not supported"
 end
 
-source url: "http://crystal-lang.s3.amazonaws.com/llvm/llvm-#{version}-#{ohai['os']}-#{ohai['kernel']['machine']}.tar.gz",
+platform = ohai['os']
+# Currently, it is considered `universal` based on the alterations made in the commit 'ed5f1f97e0a67157d886dd6675902e35199a1ab3'
+arch = "x86_64"
+
+source url: "http://crystal-lang.s3.amazonaws.com/llvm/llvm-#{version}-#{platform}-#{arch}.tar.gz",
        md5: source_md5
 
 relative_path "llvm-#{version}"
