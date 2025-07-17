3a3f48f0d3c8c34c88b16dfd40e632bc08766bdc
Matías García Isaía <mgarcia@manas.tech>
2025-07-17 00:55:31 -0300

    Move function to be post-S3-request

--- docs/aws-cloudfront-redirect.js
+++ docs/aws-cloudfront-redirect.js
@@ -1,21 +1,29 @@
+function redirect(path) {
+  return {
+    statusCode: 302,
+    headers: {
+      'location': { value: path }
+    }
+  }
+}
+
 function handler(event) {
-    var requestPath = event.request.uri
-    var redirectUri = null
+  if (event.response.statusCode != 404) {
+    // We'll only handle some 404 errors
+    return event.response
+  }
 
-    if(requestPath.startsWith('/api/latest/')) {
-      redirectUri = requestPath.replace('/api/latest/', '/api/${CRYSTAL_VERSION}/')
-    } else if(requestPath.startsWith('/api/') && !(requestPath.startsWith('/api/0') || requestPath.startsWith('/api/1'))) {
-      redirectUri = requestPath.replace('/api/', '/api/${CRYSTAL_VERSION}/')
-    }
+  var requestPath = event.request.uri
 
-    if(redirectUri != null) {
-      return {
-        statusCode: 302,
-        headers: {
-          'location': { value: redirectUri }
-        }
-      }
-    }
+  if(requestPath == '/api' || requestPath == '/api/') {
+    return redirect('/api/${CRYSTAL_VERSION}/')
+  }
 
-    return event.request
+  if(requestPath.startsWith('/api/latest/')) {
+    return redirect(requestPath.replace('/api/latest/', '/api/${CRYSTAL_VERSION}/'))
+  } else if(requestPath.startsWith('/api/') && !(requestPath.startsWith('/api/0') || requestPath.startsWith('/api/1') || requestPath.startsWith('/api/master/'))) {
+    return redirect(requestPath.replace('/api/', '/api/${CRYSTAL_VERSION}/'))
+  }
+
+  return event.response
 }
