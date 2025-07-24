9e56b5437075df2ba6a67ab261a0a9db3cf74c90
Matías García Isaía <mgarcia@manas.tech>
2025-07-24 19:40:48 -0300

    Make the function work upon viewer request
    CloudFront doesn't run viewer response functions when the origin
    responds a 4xx or 5xx, so we have to hard-code which requests can pass
    straight to the origin.
    
    This function will need to get updated if we add extra files to the
    `/api/` directory, such as a 2.x release, or other new files - I predict
    this will be the source of a few bugs in the future.

--- docs/aws-cloudfront-redirect.js
+++ docs/aws-cloudfront-redirect.js
@@ -1,3 +1,6 @@
+// Since CloudFront doesn't run functions when the origin returns a statusCode >=400
+// this function has to run upon "viewer request", before hitting the origin
+
 function redirect(path) {
   return {
     statusCode: 302,
@@ -10,22 +13,29 @@ function redirect(path) {
 const LATEST_INDEX_REDIRECTION_PATHS = [ '/api', '/api/', '/api/index.html', '/api/latest']
 
 function handler(event) {
-  if (event.response.statusCode != 404) {
-    // We'll only handle some 404 errors
-    return event.response
-  }
-
   var requestPath = event.request.uri
 
+  // well-known paths that should be queried as-is
+  if(
+    requestPath == '/api/versions.json' ||
+    requestPath.startsWith('/api/1.') ||
+    requestPath.startsWith('/api/0.') ||
+    requestPath == '/api/master' ||
+    requestPath.startsWith('/api/master/')
+  ) {
+    return event.request
+  }
+
+  // prefixes that go to the current version's index
   if(LATEST_INDEX_REDIRECTION_PATHS.includes(requestPath)) {
     return redirect('/api/${CRYSTAL_VERSION}/')
   }
 
+  // "latest" prefix should be re-written to the current version
   if(requestPath.startsWith('/api/latest/')) {
     return redirect(requestPath.replace('/api/latest/', '/api/${CRYSTAL_VERSION}/'))
-  } else if(requestPath.startsWith('/api/') && !(requestPath.startsWith('/api/0') || requestPath.startsWith('/api/1') || requestPath.startsWith('/api/master/'))) {
-    return redirect(requestPath.replace('/api/', '/api/${CRYSTAL_VERSION}/'))
   }
 
-  return event.response
+  // at this point, we assume the request omited the version - eg, `/api/String.html`
+  return redirect(requestPath.replace('/api/', '/api/${CRYSTAL_VERSION}/'))
 }
