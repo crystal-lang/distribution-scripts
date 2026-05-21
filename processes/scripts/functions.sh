ab2e3e1ccdb7144e1dfa3bc74e615eed020be695
Johannes Müller <straightshoota@gmail.com>
2026-05-21 19:49:57 +0200

    Fix SC3037: echo flags are undefined

--- processes/scripts/functions.sh
+++ processes/scripts/functions.sh
@@ -7,13 +7,13 @@ step(){
   command="$*"
 
   echo
-  echo -e "\033[33m===============================================================================\033[0m"
-  echo -e "\033[33m$step_number. $message\033[0m"
+  printf "\033[33m===============================================================================\033[0m\n"
+  printf "\033[33m%2d. %s\033[0m\n" "$step_number" "$message"
   echo
-  echo -en "$ $command"
+  printf "$ %s" "$command"
   step_number=$(expr $step_number + 1)
   if [ "$step_number" -lt "$START_STEP" ]; then
-    echo -e " \033[33m(skipped)\033[0m"
+    printf " \033[33m(skipped)\033[0m\n"
     return
   fi
 
@@ -22,6 +22,6 @@ step(){
   if [ "$REPLY" != "skip" ]; then
     eval "$command"
   else
-    echo -e "\033[33m(skipped)\033[0m"
+    printf "\033[33m(skipped)\033[0m\n"
   fi
 }
