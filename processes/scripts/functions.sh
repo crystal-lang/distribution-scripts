ab2c98b2a735bdf3ff941533552dde75088b0761
Johannes Müller <straightshoota@gmail.com>
2026-05-21 19:50:11 +0200

    Fix SC2003: expr is antiquated

--- processes/scripts/functions.sh
+++ processes/scripts/functions.sh
@@ -11,7 +11,7 @@ step(){
   printf "\033[33m%2d. %s\033[0m\n" "$step_number" "$message"
   echo
   printf "$ %s" "$command"
-  step_number=$(expr $step_number + 1)
+  step_number=$((step_number + 1))
   if [ "$step_number" -lt "$START_STEP" ]; then
     printf " \033[33m(skipped)\033[0m\n"
     return
