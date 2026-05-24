bb740e9e95afab56e97e4021e23c04589ca01f28
Johannes Müller <straightshoota@gmail.com>
2026-05-24 14:06:39 +0200

    Fix make `smoke.sh` POSIX compatible

--- docker/smoke.sh
+++ docker/smoke.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Smoke tests for Crystal docker images
 #
@@ -6,7 +6,7 @@
 
 set -eux
 
-if [[ "$VERSION" =~ ^[0-9]+(\.[0-9]+)*$ ]]; then
+if printf '%s\n' "$VERSION" | grep -Eq '^[0-9]+([.][0-9]+)*$'; then
   crystal --version | grep -q "${VERSION}"
 else
   # $VERSION is not a version number on maintenance builds
@@ -17,8 +17,8 @@ shards --version | grep -q Shards
 
 case "$1" in
   *-build)
-    llvm_config=(/usr/bin/llvm-config-*)
-    crystal eval 'require "llvm"; puts LLVM.version' | grep -q "$("${llvm_config[0]}" --version)"
+    set -- /usr/bin/llvm-config-*
+    crystal eval 'require "llvm"; puts LLVM.version' | grep -q "$("$1" --version)"
     ;;
   *)
     crystal eval 'puts "Hello World"' | grep -q "Hello World"
